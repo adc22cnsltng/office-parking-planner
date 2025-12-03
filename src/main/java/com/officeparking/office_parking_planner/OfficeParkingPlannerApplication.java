8ab0cee0be6839daa2e135f3fc745f963b56635e
adc22cnsltng <a.daddario@c22.consulting>
2025-12-03 10:55:44 +0100

    init

---
+++ src/main/java/com/officeparking/office_parking_planner/OfficeParkingPlannerApplication.java
@@ -0,0 +1,13 @@
+package com.officeparking.office_parking_planner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OfficeParkingPlannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OfficeParkingPlannerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/officeparking/office_parking_planner/OfficeParkingPlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.officeparking.office_parking_planner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OfficeParkingPlannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
